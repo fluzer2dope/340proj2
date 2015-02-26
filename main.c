695cfbc075df6edefbfbf7f3ccf688664d1d1d72
Mathias S. Tabor <mathias.tabor@gmail.com>
2015-02-25 20:43:20 -0500

    main.c comments; main.c now compiles
    Previous commit didn't update main.c
    * Added header comments to main.c with project/group info
    * Slight edits to main.c so it now compiles on Unix lab computers
      (was using C++-style input/output)

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
-#include <iostream>
+/* 
+** FILE: main.c
+** NAME: Daosavanh, Jameson; Markoff, Aaron; Tabor, Mathias (Group D)
+** GOAL: (Project 2) Main program logic:
+**	Take input from stdin, execute required commands
+*/
 
-using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
 
 int main() {
-    cout << "Hello, World!" << endl;
+    printf("Hello, World!\r\n");
     return 0;
-
 }
